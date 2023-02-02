b751915d8e0f6b2a5d6451a3e3ddb9a809f721ce
Dgalus <damiangalus7@gmail.com>
2023-02-01 20:25:15 -0500

    Added logic to render license badge and link

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,4 +1,4 @@
-const licArr = ['MIT', 'GNU GPL', 'Apache License 2.0', 'Mozilla Public License 2.0'];
+const licArr = ['MIT', 'GNU GPL 2.0', 'Apache License 2.0', 'Mozilla Public License 2.0'];
 
 
 // TODO: Create a function that returns a license badge based on which license is passed in
@@ -21,13 +21,23 @@ function renderLicenseBadge(license) {
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
   if(license === licArr[0]) {
-
+    return "https://opensource.org/licenses/MIT"
+  } else if(license === licArr[1]) {
+    return "https://opensource.org/licenses/gpl-2.0.php"
+  } else if(license === licArr[2]) {
+    return "https://opensource.org/licenses/Apache-2.0"
+  } else if(license === licArr[3]) {
+    return "https://opensource.org/licenses/MPL-2.0"
+  } else {
+    return ""
   }
 }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
